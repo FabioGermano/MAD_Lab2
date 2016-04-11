d23c5266e33d1bd9015db3c0ffd899744decd8d6
FabioGermano <gefasio@gmail.com>
2016-04-11 23:53:25 +0200

    ora possibile salvare foto in prifle editing

--- app/src/main/java/it/polito/mad_lab2/EditRestaurantProfile.java
+++ app/src/main/java/it/polito/mad_lab2/EditRestaurantProfile.java
@@ -1,14 +1,19 @@
 package it.polito.mad_lab2;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Switch;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import org.json.JSONArray;
@@ -84,17 +89,22 @@ public class EditRestaurantProfile extends BaseActivity implements PhotoViewerLi
             String profilo = DB.leggiDB(this, "db_profilo");
 
             JSONObject jsonRootObject = new JSONObject(profilo);
-            JSONArray jsonArray = jsonRootObject.getJSONArray("profilo");
-            JSONObject jsonObject = jsonArray.getJSONObject(0);
 
-            String name = jsonObject.optString("nome").toString();
-            String address = jsonObject.optString("indirizzo").toString();
-            String phone = jsonObject.optString("telefono");
-            String email =jsonObject.optString("email");
-            String description = jsonObject.optString("descrizione");
+            JSONArray jsonArray = jsonRootObject.optJSONArray("profilo");
+
+            JSONObject info = jsonArray.getJSONObject(0);
+            JSONObject orari = jsonArray.getJSONObject(1);
+            JSONObject features = jsonArray.getJSONObject(4);
+
+
+            String name = info.optString("nome").toString();
+            String address = info.optString("indirizzo").toString();
+            String phone = info.optString("telefono");
+            String email =info.optString("email");
+            String description = info.optString("descrizione");
 
             // read logo path
-            this.logoThumbPath = jsonObject.optString("logoThumbPath");
+            this.logoThumbPath = info.optString("logoThumbPath");
             if(this.logoThumbPath.equals("")){
                 this.logoThumbPath = null;
             }
@@ -105,17 +115,55 @@ public class EditRestaurantProfile extends BaseActivity implements PhotoViewerLi
 
             // read cover path
             for(int i = 0; i<4; i++){
-                this.coversThumbPath[i] = jsonObject.optString("coversThumbPath_"+i);
+                this.coversThumbPath[i] = info.optString("coversThumbPath_"+i);
                 if(this.coversThumbPath[i].equals("")){
                     this.coversThumbPath[i] = null;
                 }
             }
             for(int i = 0; i<4; i++){
-                this.coversLargePath[i] = jsonObject.optString("coversLargePath_"+i);
+                this.coversLargePath[i] = info.optString("coversLargePath_"+i);
                 if(this.coversLargePath[i].equals("")){
                     this.coversLargePath[i] = null;
                 }
             }
+
+            String lun = getResources().getString(R.string.monday).toUpperCase();
+            if (orari.optString("lun").compareTo("null") != 0){
+                lun += "\n" + orari.optString("lun");
+            }
+            String mar = getResources().getString(R.string.tuesday).toUpperCase();
+            if (orari.optString("mar").compareTo("null") != 0){
+                mar += "\n" + orari.optString("mar");
+            }
+            String mer = getResources().getString(R.string.wednesday).toUpperCase();
+            if (orari.optString("mer").compareTo("null") != 0){
+                mer += "\n" + orari.optString("mer");
+            }
+            String gio = getResources().getString(R.string.thursday).toUpperCase();
+            if (orari.optString("gio").compareTo("null") != 0){
+                gio += "\n" + orari.optString("gio");
+            }
+            String ven = getResources().getString(R.string.friday).toUpperCase();
+            if (orari.optString("ven").compareTo("null") != 0){
+                ven += "\n" + orari.optString("ven");
+            }
+            String sab = getResources().getString(R.string.saturday).toUpperCase();
+            if (orari.optString("sab").compareTo("null") != 0){
+                sab += "\n" + orari.optString("sab");
+            }
+            String dom = getResources().getString(R.string.sunday).toUpperCase();
+            if (orari.optString("dom").compareTo("null") != 0){
+                dom += "\n" + orari.optString("dom");
+            }
+
+            boolean res = features.optBoolean("reservations");
+            boolean wifi = features.optBoolean("wifi");
+            boolean seats = features.optBoolean("seats");
+            boolean card = features.optBoolean("creditCard");
+            boolean bancomat = features.optBoolean("bancomat");
+            boolean music = features.optBoolean("music");
+            boolean park = features.optBoolean("parking");
+
 
             EditText edit_name = (EditText) findViewById(R.id.edit_name);
             EditText edit_address = (EditText) findViewById(R.id.edit_address);
@@ -123,23 +171,53 @@ public class EditRestaurantProfile extends BaseActivity implements PhotoViewerLi
             EditText edit_email = (EditText) findViewById(R.id.edit_email);
             EditText edit_description = (EditText) findViewById(R.id.edit_description);
 
-            if (edit_name != null){
-                edit_name.setText(name);
-            }
-            if (edit_address != null){
-                edit_address.setText(address);
-            }
-            if (edit_phone != null){
-                edit_phone.setText(phone);
-            }
-            if (edit_email != null){
-                edit_email.setText(email);
-            }
-            if (edit_description != null){
-                edit_description.setText(description);
-            }
+            Button lunBtn = (Button) findViewById(R.id.monday_butt);
+            Button marBtn = (Button) findViewById(R.id.tuesday_butt);
+            Button merBtn = (Button) findViewById(R.id.wednesday_butt);
+            Button gioBtn = (Button) findViewById(R.id.thursday_butt);
+            Button venBtn = (Button) findViewById(R.id.friday_butt);
+            Button sabBtn = (Button) findViewById(R.id.saturday_butt);
+            Button domBtn = (Button) findViewById(R.id.sunday_butt);
+
+            Switch resSwitch = (Switch) findViewById(R.id.reservation);
+            Switch wifiSwitch = (Switch) findViewById(R.id.wifi);
+            Switch seatsSwitch = (Switch) findViewById(R.id.seats);
+            Switch creditSwitch = (Switch) findViewById(R.id.creditcard);
+            Switch bancomatSwitch = (Switch) findViewById(R.id.bancomat);
+            Switch musicSwitch = (Switch) findViewById(R.id.music);
+            Switch parkSwitch = (Switch) findViewById(R.id.parking);
+
+
+
+
+            if (edit_name != null){ edit_name.setText(name); }
+            if (edit_address != null){ edit_address.setText(address); }
+            if (edit_phone != null){ edit_phone.setText(phone); }
+            if (edit_email != null){ edit_email.setText(email); }
+            if (edit_description != null){ edit_description.setText(description); }
+
+            if (lunBtn != null){ lunBtn.setText(lun); }
+            if (marBtn != null){ marBtn.setText(mar); }
+            if (merBtn != null){ merBtn.setText(mer); }
+            if (gioBtn != null){ gioBtn.setText(gio); }
+            if (venBtn != null){ venBtn.setText(ven); }
+            if (sabBtn != null){ sabBtn.setText(sab); }
+            if (domBtn != null){ domBtn.setText(dom); }
+
+            if (resSwitch != null) { resSwitch.setChecked(res); };
+            if (wifiSwitch != null) { wifiSwitch.setChecked(wifi); };
+            if (seatsSwitch != null) { seatsSwitch.setChecked(seats); };
+            if (creditSwitch != null) { creditSwitch.setChecked(card); };
+            if (bancomatSwitch != null) { bancomatSwitch.setChecked(bancomat); };
+            if (musicSwitch != null) { musicSwitch.setChecked(music); };
+            if (parkSwitch != null) { parkSwitch.setChecked(park); };
+
+
         }
         catch(Exception e){
+
+            Toast toast = Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT);
+            toast.show();
             System.out.println("Eccezione: "+e.getMessage());
         }
     }
@@ -147,7 +225,8 @@ public class EditRestaurantProfile extends BaseActivity implements PhotoViewerLi
     private boolean saveInfo(){
 
         try {
-            String name, address, phone, email, description;
+            String name, address, phone, email, description, tmp;
+            String lun, mar, mer, gio, ven, sab, dom;
 
             EditText edit_name = (EditText) findViewById(R.id.edit_name);
             EditText edit_address = (EditText) findViewById(R.id.edit_address);
@@ -155,11 +234,107 @@ public class EditRestaurantProfile extends BaseActivity implements PhotoViewerLi
             EditText edit_email = (EditText) findViewById(R.id.edit_email);
             EditText edit_description = (EditText) findViewById(R.id.edit_description);
 
+            Button lunBtn = (Button) findViewById(R.id.monday_butt);
+            Button marBtn = (Button) findViewById(R.id.tuesday_butt);
+            Button merBtn = (Button) findViewById(R.id.wednesday_butt);
+            Button gioBtn = (Button) findViewById(R.id.thursday_butt);
+            Button venBtn = (Button) findViewById(R.id.friday_butt);
+            Button sabBtn = (Button) findViewById(R.id.saturday_butt);
+            Button domBtn = (Button) findViewById(R.id.sunday_butt);
+
+            Switch resSwitch = (Switch) findViewById(R.id.reservation);
+            Switch wifiSwitch = (Switch) findViewById(R.id.wifi);
+            Switch seatsSwitch = (Switch) findViewById(R.id.seats);
+            Switch creditSwitch = (Switch) findViewById(R.id.creditcard);
+            Switch bancomatSwitch = (Switch) findViewById(R.id.bancomat);
+            Switch musicSwitch = (Switch) findViewById(R.id.music);
+            Switch parkSwitch = (Switch) findViewById(R.id.parking);
+
+
+
+            if (lunBtn != null){
+                tmp = lunBtn.getText().toString();
+                if (tmp.compareTo(getResources().getString(R.string.monday)) == 0){
+                    lun = "";
+                }
+                else
+                    lun = tmp.substring(tmp.indexOf("\n")+1,tmp.length());
+            }
+            else
+                lun = null;
+
+            if (marBtn != null){
+                tmp = marBtn.getText().toString();
+                if (tmp.compareTo(getResources().getString(R.string.tuesday)) == 0){
+                    mar = "";
+                }
+                else
+                    mar = tmp.substring(tmp.indexOf("\n")+1,tmp.length());
+            }
+            else
+                mar = null;
+
+
+            if (merBtn != null) {
+                tmp = merBtn.getText().toString();
+                if (tmp.compareTo(getResources().getString(R.string.wednesday)) == 0) {
+                    mer = "";
+                } else
+                    mer = tmp.substring(tmp.indexOf("\n") + 1, tmp.length());
+            }
+            else
+                mer = null;
+
+            if (gioBtn != null){
+                tmp = gioBtn.getText().toString();
+                if (tmp.compareTo(getResources().getString(R.string.thursday)) == 0){
+                    gio = "";
+                }
+                else
+                    gio = tmp.substring(tmp.indexOf("\n")+1,tmp.length());
+            }
+            else
+                gio = null;
+
+            if (venBtn != null){
+                tmp = venBtn.getText().toString();
+                if (tmp.compareTo(getResources().getString(R.string.friday)) == 0){
+                    ven = "";
+                }
+                else
+                    ven = tmp.substring(tmp.indexOf("\n")+1,tmp.length());
+            }
+            else
+                ven = null;
+
+            if (sabBtn != null){
+                tmp = sabBtn.getText().toString();
+                if (tmp.compareTo(getResources().getString(R.string.saturday)) == 0){
+                    sab = "";
+                }
+                else
+                    sab = tmp.substring(tmp.indexOf("\n")+1,tmp.length());
+            }
+            else
+                sab = null;
+
+            if (domBtn != null){
+                tmp = domBtn.getText().toString();
+                if (tmp.compareTo(getResources().getString(R.string.sunday)) == 0){
+                    dom = "";
+                }
+                else
+                    dom = tmp.substring(tmp.indexOf("\n")+1,tmp.length());
+            }
+            else
+                dom = null;
+
+
             if (edit_name != null) {
                 name = edit_name.getText().toString();
             }
             else
-                name= null;
+                name = null;
             if (edit_address != null) {
                 address = edit_address.getText().toString();
             }
@@ -181,26 +356,39 @@ public class EditRestaurantProfile extends BaseActivity implements PhotoViewerLi
             else
                 description = null;
 
-            if (name != null && address != null && phone != null && email != null && description != null){
+            if (name != null && address != null && phone != null && email != null && description != null && lun != null && mar != null && mer != null && gio != null && ven != null && sab != null && dom != null && resSwitch != null &&
+                    wifiSwitch != null && seatsSwitch != null && creditSwitch != null && bancomatSwitch != null && musicSwitch != null && parkSwitch != null){
+
+                if (name.isEmpty() || address.isEmpty() || phone.isEmpty() || email.isEmpty() || description.isEmpty() || lun.isEmpty() || mar.isEmpty() || mer.isEmpty() || gio.isEmpty() || ven.isEmpty() || sab.isEmpty() || dom.isEmpty()){
+                    AlertDialog.Builder miaAlert = new AlertDialog.Builder(this);
+                    miaAlert.setTitle(getResources().getString(R.string.error));
+                    miaAlert.setMessage(getResources().getString(R.string.error_complete));
+                    AlertDialog alert = miaAlert.create();
+                    alert.show();
+                    return false;
+                }
+
                 GestioneDB DB = new GestioneDB();
                 String profilo = DB.leggiDB(this, "db_profilo");
 
                 JSONObject jsonRootObject = new JSONObject(profilo);
                 JSONArray jsonArray = jsonRootObject.getJSONArray("profilo");
-                JSONObject jsonObject = jsonArray.getJSONObject(0);
+                JSONObject info = jsonArray.getJSONObject(0);
+                JSONObject orari = jsonArray.getJSONObject(1);
+                JSONObject features = jsonArray.getJSONObject(4);
 
-                jsonObject.put("nome", name);
-                jsonObject.put("indirizzo", address);
-                jsonObject.put("telefono", phone);
-                jsonObject.put("email", email);
-                jsonObject.put("descrizione", description);
+                info.put("nome", name);
+                info.put("indirizzo", address);
+                info.put("telefono", phone);
+                info.put("email", email);
+                info.put("descrizione", description);
 
                 //save logo paths
                 if(this.logoThumbPath != null)
                 {
-                    jsonObject.put("logoThumbPath", this.logoThumbPath);
+                    info.put("logoThumbPath", this.logoThumbPath);
                 }
-               // if(this.logoLargePath != null)
+                // if(this.logoLargePath != null)
                 //{
                 //    jsonObject.put("logoLargePath", this.logoLargePath);
                 //}
@@ -208,20 +396,43 @@ public class EditRestaurantProfile extends BaseActivity implements PhotoViewerLi
                 //save cover path
                 for(int i = 0; i<4; i++){
                     if(this.coversThumbPath[i] != null) {
-                        jsonObject.put("coversThumbPath_" + i, this.coversThumbPath[i]);
+                        info.put("coversThumbPath_" + i, this.coversThumbPath[i]);
                     }
                 }
                 for(int i = 0; i<4; i++){
                     if(this.coversLargePath[i] != null) {
-                        jsonObject.put("coversLargePath_" + i, this.coversLargePath[i]);
+                        info.put("coversLargePath_" + i, this.coversLargePath[i]);
                     }
                 }
+
+                orari.put("lun", lun);
+                orari.put("mar", mar);
+                orari.put("mer", mer);
+                orari.put("gio", gio);
+                orari.put("ven", ven);
+                orari.put("sab", sab);
+                orari.put("dom", dom);
+                features.put("reservations", resSwitch.isChecked());
+                features.put("wifi", wifiSwitch.isChecked());
+                features.put("seats", seatsSwitch.isChecked());
+                features.put("creditCard", creditSwitch.isChecked());
+                features.put("bancomat", bancomatSwitch.isChecked());
+                features.put("music", musicSwitch.isChecked());
+                features.put("parking", parkSwitch.isChecked());
+
 
                 DB.updateDB(this, jsonRootObject, "db_profilo");
                 return true;
             }
-            else
+            else{
+                AlertDialog.Builder miaAlert = new AlertDialog.Builder(this);
+                miaAlert.setTitle(getResources().getString(R.string.error));
+                miaAlert.setMessage(getResources().getString(R.string.exceptionError));
+                AlertDialog alert = miaAlert.create();
+                alert.show();
                 return false;
+            }
+
         }
         catch (Exception e){
             System.out.println("Eccezione: "+e.getMessage());
@@ -243,14 +454,18 @@ public class EditRestaurantProfile extends BaseActivity implements PhotoViewerLi
 
     @Override
     protected void OnSaveButtonPressed() {
+        System.out.println("ORARIO LUNEDI: "/* + monday.getText()*/);
+
         commitPhotos();
         boolean ris = saveInfo();
+
         if(ris) {
             Toast toast = Toast.makeText(getApplicationContext(), R.string.dataSaved, Toast.LENGTH_SHORT);
             toast.show();
             Intent intent = new Intent(getApplicationContext(), MainActivity.class);
             startActivity(intent);
         }
+
     }
 
     @Override
