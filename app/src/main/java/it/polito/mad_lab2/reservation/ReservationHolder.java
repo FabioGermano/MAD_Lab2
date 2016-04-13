833d6375850132aea82aa345fdc35ea1d335919f
FabioGermano <gefasio@gmail.com>
2016-04-13 19:43:09 +0200

    forse risolto sputtanamento continu with reservations

--- app/src/main/java/it/polito/mad_lab2/reservation/ReservationHolder.java
+++ app/src/main/java/it/polito/mad_lab2/reservation/ReservationHolder.java
@@ -1,15 +1,22 @@
 package it.polito.mad_lab2.reservation;
 
 import android.content.Context;
+import android.graphics.Typeface;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import junit.framework.Test;
 
+import java.util.ArrayList;
+
 import it.polito.mad_lab2.R;
 import it.polito.mad_lab2.data.reservation.Reservation;
+import it.polito.mad_lab2.data.reservation.ReservedDish;
 
 /**
  * Created by Giovanna on 12/04/2016.
@@ -19,7 +26,7 @@ public class ReservationHolder extends RecyclerView.ViewHolder {
     private Context context;
     private Reservation reservation;
     private TextView username, type, time;
-    private ListView reservedDishListview, reservedOfferListview;
+    private LinearLayout offerListLayout, dishListLayout;
 
     public ReservationHolder(View v, Context context) {
         super(v);
@@ -30,20 +37,59 @@ public class ReservationHolder extends RecyclerView.ViewHolder {
         type = (TextView) v.findViewById(R.id.type);
         time = (TextView) v.findViewById(R.id.time);
 
-        reservedDishListview = (ListView)v.findViewById(R.id.listView_dishes);
-        reservedOfferListview = (ListView)v.findViewById(R.id.listView_offers);
+        offerListLayout = (LinearLayout)v.findViewById(R.id.offerListLayout);
+        dishListLayout = (LinearLayout)v.findViewById(R.id.dishListLayout);
     }
 
     public void setData(Reservation reservation){
         this.reservation=reservation;
         type.setText(reservation.getType());
         username.setText(reservation.getUser().getName());
-        time.setText(reservation.getDate());
+        time.setText(reservation.getTime());
 
-        ReservedDishListAdapter listAdapterDish = new ReservedDishListAdapter(this.context, reservation.getReservedDishes(false));
-        reservedDishListview.setAdapter(listAdapterDish);
+        ArrayList<ReservedDish> reservedDish = reservation.getReservedDishes(false);
+        ArrayList<ReservedDish> reservedOffers = reservation.getReservedDishes(true);
 
-        ReservedDishListAdapter listAdapterOffer = new ReservedDishListAdapter(this.context, reservation.getReservedDishes(true));
-        reservedOfferListview.setAdapter(listAdapterOffer);
+        if(reservedDish.size() > 0) {
+            View child = LayoutInflater.from(context).inflate(R.layout.order_row, null);
+            TextView name = (TextView) child.findViewById(R.id.food_name);
+            name.setText("Name");
+            name.setTypeface(name.getTypeface(), Typeface.NORMAL);
+            TextView quantity = (TextView) child.findViewById(R.id.quantity);
+            quantity.setText("Quantity");
+            quantity.setTypeface(name.getTypeface(), Typeface.NORMAL);
+            this.dishListLayout.addView(child);
+
+            for (ReservedDish rd : reservedDish) {
+                child = LayoutInflater.from(context).inflate(R.layout.order_row, null);
+                name = (TextView) child.findViewById(R.id.food_name);
+                name.setText(rd.getName());
+                quantity = (TextView) child.findViewById(R.id.quantity);
+                quantity.setText(String.valueOf(rd.getQuantity()));
+
+                this.dishListLayout.addView(child);
+            }
+        }
+
+        if(reservedDish.size() > 0) {
+            View child = LayoutInflater.from(context).inflate(R.layout.order_row, null);
+            TextView name = (TextView) child.findViewById(R.id.food_name);
+            name.setText("Name");
+            name.setTypeface(name.getTypeface(), Typeface.NORMAL);
+            TextView quantity = (TextView) child.findViewById(R.id.quantity);
+            quantity.setText("Quantity");
+            quantity.setTypeface(name.getTypeface(), Typeface.NORMAL);
+            this.offerListLayout.addView(child);
+
+            for (ReservedDish rd : reservedOffers) {
+                child = LayoutInflater.from(context).inflate(R.layout.order_row, null);
+                name = (TextView) child.findViewById(R.id.food_name);
+                name.setText(rd.getName());
+                quantity = (TextView) child.findViewById(R.id.quantity);
+                quantity.setText(String.valueOf(rd.getQuantity()));
+
+                this.offerListLayout.addView(child);
+            }
+        }
     }
 }
